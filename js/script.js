1819987852b6d5a1f7f01522f596ec8e3ac4f01c
rc2pc2 <petriccarcc@gmail.com>
2024-05-27 10:18:13 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+infoTime();
+const somma = (numA, numB) => numA + numB;
+
+console.log(somma(2,4));
+console.log(somma(14,15));
+console.log(somma(1497,1532));
+
+
+function infoTime(){
+    console.log(new Date());
+}
+
+// function somma(numA, numB){
+//     return numA + numB;
+// }
